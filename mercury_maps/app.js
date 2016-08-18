d9b047cf058785e02896af4667fd882a59186cd2
MuffinsaursRex <jackyfryers@gmail.com>
2016-08-17 20:36:25 -0700

    working send localtable, need to implement recieve table and updated local table

--- mercury_maps/app.js
+++ mercury_maps/app.js
@@ -4,8 +4,69 @@ var devId = hexToBytes(decimalToHexString(process.argv[2]));
 devId = devId[0]
 console.log("devId: ", devId);
 
+var localTable = [];
+var recievedTable = [];
+
+//payload/device:		| devId |    Lat    |    Lon    |      Time      |
+//propertiesize:	 		1		  4			  4				6 			bytes
+//payloadsize/device: 15
+devIdsize = 1;
+latSize  = 4;
+lonSize = 4;
+timeSize = 6;
+var devicePayloadSize = devIdsize + latSize + lonSize + timeSize;
+
 
 /*******Lora Payload Functions*******/
+(function(){
+    var ConvertBase = function (num) {
+        return {
+            from : function (baseFrom) {
+                return {
+                    to : function (baseTo) {
+                        return parseInt(num, baseFrom).toString(baseTo);
+                    }
+                };
+            }
+        };
+    };
+        
+    // binary to decimal
+    ConvertBase.bin2dec = function (num) {
+        return ConvertBase(num).from(2).to(10);
+    };
+    
+    // binary to hexadecimal
+    ConvertBase.bin2hex = function (num) {
+        return ConvertBase(num).from(2).to(16);
+    };
+    
+    // decimal to binary
+    ConvertBase.dec2bin = function (num) {
+        return ConvertBase(num).from(10).to(2);
+    };
+    
+    // decimal to hexadecimal
+    ConvertBase.dec2hex = function (num) {
+        return ConvertBase(num).from(10).to(16);
+    };
+    
+    // hexadecimal to binary
+    ConvertBase.hex2bin = function (num) {
+        return ConvertBase(num).from(16).to(2);
+    };
+    
+    // hexadecimal to decimal
+    ConvertBase.hex2dec = function (num) {
+        return ConvertBase(num).from(16).to(10);
+    };
+    
+    this.ConvertBase = ConvertBase;
+    
+})(this);
+
+
+
 //convert signed decimal to signed hex string
 function decimalToHexString(number)
 {
@@ -44,7 +105,87 @@ function bytesToHex(bytes) {
     }
     return hex.join("");
 }
+
+// take id, lat lon, and time and turns it into a byte array
+function makePayload(id,lat,lon,time){ 
+	//data from SX127x / socketio
+	var payload = new Array()
+	
+	//add devId to payload
+	payload[0] = id //add device id to the first bye
+	
+	//Change Lat / Lon to a non decimal number
+	lat = Math.round(lat * 10000000)
+	lon = Math.round(lon * 10000000)
+	
+	//convert decimal to hex string
+	latHexStr = decimalToHexString(lat)
+	//convert hex string to byte array
+	latByte =  hexToBytes(latHexStr)
+	
+	//convert decimal to hex string
+	lonHexStr = decimalToHexString(lon)
+	//convert hex string to byte array
+	lonByte =  hexToBytes(lonHexStr)
+	
+	
+	//fill out longitude in payload
+	for(var i = 0; i < latByte.length; i++){
+	   payload[i + devIdsize]  = latByte[i]
+	}
+	//fill out latitude in payload
+	for(var i = 0; i < lonByte.length; i++){
+	   payload[i + devIdsize + latSize]  = lonByte[i]
+	}
+	
+	//convert time to hext string
+	timeHexStr = ConvertBase.dec2hex(time);
+	//convert hext string to byte array
+	var timeByte = new Array();
+	for(var i = 0; i < timeHexStr.length; i=i+2){
+		timeByte.push(parseInt(timeHexStr.slice(i, i+2),16))
+	}
+	//add bye array to payload
+	for(var i = 0; i < timeByte.length; i++){
+		payload[i + devIdsize + latSize + lonSize]  = timeByte[i]	
+	}
+	//console.log('sending data_sx127x:',payload);
+	return payload
+}
+
 /*******END-Lora Payload Functions*******/
+//convert time portion of payload to time
+function timePayloadToTime(timePayload){
+	timeStr = "";
+	//byte array to hex string
+	for(var i = 0; i < timePayload.length; i++){
+		timeStr += timePayload[i].toString(16)
+	}
+	//hex string to decimal
+	var time = new Date(ConvertBase.hex2dec(timeStr)).getTime();
+	return time;
+}
+
+//take a payload and return recievedTable
+function decodePayload(payload){
+	//filter out packets that do not correspond with payload sizes.
+	if(payload.length % devicePayloadSize === 0){
+		recievedTable = new Array();
+		for(var i = 0; i < payload.length / devicePayloadSize; i++){
+			var id = payload[i * (payload.length / (i + 1))];
+			var lat = (hexToInt(bytesToHex(payload.slice(i * (payload.length / (i + 1)) + devIdsize, i * (payload.length / (i + 1)) + devIdsize + latSize))) / 10000000);
+			var lon = (hexToInt(bytesToHex(payload.slice(i * (payload.length / (i + 1)) + devIdsize + latSize, i * (payload.length / (i + 1)) + devIdsize + latSize + lonSize))) / 10000000);
+			var timePayload = timePayloadToTime(payload.slice(i * (payload.length / (i + 1)) + devIdsize + latSize + lonSize, i * (payload.length / (i + 1)) + devIdsize + latSize + lonSize + timeSize)); 
+			var tempDevice = new device(id,lat,lon,time);
+			recievedTable.push(tempDevice);
+		}
+		return recievedTable;
+	}
+	else{
+		console.log("not the packet we are looking for");
+	}
+}
+
 //Create devie object
 function device(id, lat, lon, time) {
     this.id = id;
@@ -54,9 +195,7 @@ function device(id, lat, lon, time) {
 };
 
 //compare local table to recieved table and return updated table
-var localTable = [];
-var recievedTable = [];
-function UpdateLocalTable( recievedTable, localTable){
+function updateLocalTable( recievedTable, localTable){
 	var newDevices = []; // will hold new found devices to be added to local table
 	for(var i = 0; i < recievedTable.length; i++){
 		var newDevice = true;
@@ -64,7 +203,7 @@ function UpdateLocalTable( recievedTable, localTable){
 			if(recievedTable[i].id == localTable[j].id) {
 				newDevice = false;
 				if(recievedTable[i].time > localTable[j].time){
-					console.log("update local table");
+					console.log("update local table", recievedTable[i]);
 					localTable[j] = recievedTable[i];
 				}
 			}
@@ -80,6 +219,21 @@ function UpdateLocalTable( recievedTable, localTable){
 	return localTable;
 };
 
+function localTableToPaload(localTable){
+	var payload = new Array()
+	for(var i = 0; i < localTable.length; i++){
+		payload = payload.concat( makePayload(localTable[i].id, localTable[i].lat, localTable[i].lon, localTable[i].time ) );
+	}
+	return payload;
+}
+
+//simulate data in from GPS
+function DataFromGps(id, lat, lon, time){
+    var myDevice = new device(id, lat, lon, time);
+    var recievedTable = [];
+    recievedTable.push(myDevice);
+    localTable = updateLocalTable(recievedTable,localTable);
+} 
 
 // create an express app
 var express = require('express'),
@@ -215,12 +369,14 @@ daemon.start(function() {
 			// Insert values into prepared statement
 			var lat = tpv.lat;
 			var lon = tpv.lon;
-			statement.run( tpv.time, lat, lon, tpv.alt, tpv.speed );
+            var time = Date.parse(tpv.time);
+            
+			statement.run(time, lat, lon, tpv.alt, tpv.speed );
 			// Execute the statement
 			statement.finalize();
 			//send info to Python socket / to broadcast
             //init payload to send
-            var payload = new Array()
+    /*         var payload = new Array()
             //Change Lat / Lon to a non decimal number
             lat = Math.round(tpv.lat * 10000000)
             lon = Math.round(tpv.lon * 10000000)
@@ -242,9 +398,18 @@ daemon.start(function() {
             //fill out latitude in payload
             for(var i = 0; i < lonByte.length; i++){
                payload[i + 1 + latByte.length]  = lonByte[i]
-            }
+            } */
+            
+            //add new GPS units to local table
+            DataFromGps(devId, lat, lon, time);
+            //convert localtable to payload
+            payload = localTableToPaload(localTable);
+            //Send payload
             console.log('sending data_sx127x:',payload);
+            
 			io.emit('data_sx127x', payload);
+            
+            
             //data for webclient
             var data = {
                 'devId': devId,
