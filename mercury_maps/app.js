6faec2a994a214b4c9849a365068f232ce2bc0a6
MuffinsaursRex <jackyfryers@gmail.com>
2016-08-24 11:38:55 -0700

    moved some functions around

--- mercury_maps/app.js
+++ mercury_maps/app.js
@@ -4,12 +4,193 @@ var devId = hexToBytes(decimalToHexString(process.argv[2]));
 devId = devId[0]
 console.log("devId: ", devId);
 
+// create an express app
+var express = require('express'),
+    app = express()
+    morgan = require('morgan'),
+	bodyParser = require('body-parser'),
+    port = process.env.PORT || 3000,
+    publicDir = require('path').join(__dirname, '/public'),
+    //socket server to web client
+    http = require('http').Server(app),
+    io = require('socket.io')(http);
+
+// add logging middleware
+app.use(morgan('dev'));
+// parsing get
+app.use(bodyParser.json());
+// add the express.static middleware to the app to
+// serve files in the specified path
+// This middleware will only call the next middleware if
+// path doesn't match the static directory
+app.use(express.static(publicDir));
+
+http.listen(port);
+
+console.log('server started on port %s', port);
+
 // http://nodejs.org/api.html#_child_processes
 var sys = require('sys')
 var exec = require('child_process').exec;
 var child;
 //exec("su -l pi -c 'env FRAMEBUFFER=/dev/fb1 startx &'");
 //exec("su -l pi -c 'xinit ./util/startB &'");
+
+
+function lastGpsCordinate(callback){
+	db.each("select time,lon,lat from gpslog order by time desc limit 1;", function (err, data) {
+		if (err) {
+			response.writeHead(500, { "Content-type": "text/html" });
+			response.end(err + "\n");
+			console.log('Error serving querying database. ' + err);
+			return;
+		}
+		callback(data);
+	});
+}
+
+app.get('/GpsData.json', function(req, res) {
+	lastGpsCordinate(function (data) {
+	  console.log(data);
+	  res.send(data);         // automatic -> application/json
+	});
+});
+
+//socket stuff
+io.on('connection', function(socket){
+    console.log('a user connected to socketIO');
+    //send the device Id to web client
+    io.emit('data_wclient_init', devId);
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('shutdown', function() {
+            child = exec("shutdown now");
+    });
+    //handle data recieved from other LoRa devices
+    socket.on('data_sx127x', function(data) {
+       
+        console.log('recieved data_sx127x:',data);
+        /* 
+        console.log('deviceID: ',data[0]);
+        console.log('lat :', hexToInt(bytesToHex(data.slice(1,5))) / 10000000)
+        console.log('lon :', hexToInt(bytesToHex(data.slice(5,9))) / 10000000) */
+        console.log('data_sx127x.length', data.length)
+        recievedPayload = decodePayload(data);
+    
+        localTable = updateLocalTable(recievedTable,localTable);
+        
+        //data for webclient
+        var data = {
+            'devId' : data[0],
+            'lat': (hexToInt(bytesToHex(data.slice(1,5))) / 10000000),
+            'lon': (hexToInt(bytesToHex(data.slice(5,9))) / 10000000),
+            'time': new Date()
+        };
+        io.emit('data_web', data);
+    });
+});
+
+
+
+
+
+
+
+//SQL
+var sqlite3 = require('sqlite3');
+TransactionDatabase = require("sqlite3-transactions").TransactionDatabase;
+// Setup database connection for logging
+var db = new TransactionDatabase(
+    new sqlite3.Database("/projects/mercury/mercury_maps/mercery.db", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
+);
+
+// Start socket server python scripts
+// executes `pkill` - make sure socketserver isnt already running
+child = exec("pkill -f sendRecieveData.py");
+
+var PythonShell = require('python-shell');
+var pyshell = new PythonShell('./pySX127x/sendRecieveData.py',{args: ['freq --433']},function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution
+  console.log('results: %j', results);
+});
+pyshell.on('message', function (message) {
+  // received a message sent from the Python script (a simple "print" statement)
+  console.log(message);
+});
+// end the input stream and allow the process to exit
+pyshell.end(function (err) {
+  if (err) throw err;
+  console.log('finished');
+});
+//start GPSD
+// executes `pkill` - make sure socketserver isnt already running
+child = exec("pkill -f gpsd");
+
+var gpsd = require('node-gpsd');
+var gpsd = require('./lib/gpsd.js');
+var previousGpsdTime = new Date(); //keep track of the last time we got a GPSD cvalue
+var GpsdInterval = 5;//how often (seconds) to use GPSD data
+var daemon = new gpsd.Daemon({
+    program: 'gpsd',
+    device: '/dev/ttyAMA0',
+    port: 2947,
+    pid: '/tmp/gpsd.pid',
+    logger: {
+        info: function() {},
+        warn: console.warn,
+        error: console.error
+    }
+});
+daemon.start(function() {
+    console.log('Started GPSD daemon');
+	var listener = new gpsd.Listener();
+    listener.on('TPV', function (tpv) {
+		//only collect data every GpsdInterval
+        //console.log(tpv);
+        //tpv.mode should be 3 since 3 means lot/lat
+		if(((new Date().getTime() - previousGpsdTime.getTime()) / 1000) > GpsdInterval && tpv.time && tpv.lat && tpv.lon && tpv.alt && tpv.speed){
+            previousGpsdTime = new Date();
+			var statement = db.prepare("INSERT INTO GpsLog VALUES (?, ?, ?, ?, ?)");
+			// Insert values into prepared statement
+			var lat = tpv.lat;
+			var lon = tpv.lon;
+            var time = Date.parse(tpv.time);
+            //console.log('parsedTpvDatetime', time)
+			statement.run(time, lat, lon, tpv.alt, tpv.speed );
+			// Execute the statement
+			statement.finalize();        
+            //add new GPS units to local table
+            DataFromGps(devId, lat, lon, time);
+            //convert localtable to payload
+            //console.log("localtable:", localTable)
+            payload = localTableToPaload(localTable);
+            //Send payload
+            console.log('sendpayload.length',payload.length);
+            console.log('sending data_sx127x:',payload);
+            
+			io.emit('data_sx127x', payload);
+            
+            
+            //data for webclient
+/*             var data = {
+                'devId': devId,
+                'lat':tpv.lat,
+                'lon':tpv.lon,
+                'time':new Date()
+            }; */
+            io.emit('data_web', localTable);
+		}
+    });
+    listener.connect(function() {
+        listener.watch();
+    });
+});
+
+
+/////////////////// Payload /////////////////// 
+
 
 var localTable = [];
 var recievedTable = [];
@@ -245,206 +426,6 @@ function DataFromGps(id, lat, lon, time){
     recievedTable.push(myDevice);
     localTable = updateLocalTable(recievedTable,localTable);
 } 
-
-// create an express app
-var express = require('express'),
-    app = express()
-    morgan = require('morgan'),
-	bodyParser = require('body-parser'),
-    port = process.env.PORT || 3000,
-    publicDir = require('path').join(__dirname, '/public'),
-    //socket server to web client
-    http = require('http').Server(app),
-    io = require('socket.io')(http);
-
-// add logging middleware
-app.use(morgan('dev'));
-// parsing get
-app.use(bodyParser.json());
-// add the express.static middleware to the app to
-// serve files in the specified path
-// This middleware will only call the next middleware if
-// path doesn't match the static directory
-app.use(express.static(publicDir));
-
-function lastGpsCordinate(callback){
-	db.each("select time,lon,lat from gpslog order by time desc limit 1;", function (err, data) {
-		if (err) {
-			response.writeHead(500, { "Content-type": "text/html" });
-			response.end(err + "\n");
-			console.log('Error serving querying database. ' + err);
-			return;
-		}
-		callback(data);
-	});
-}
-
-app.get('/GpsData.json', function(req, res) {
-	lastGpsCordinate(function (data) {
-	  console.log(data);
-	  res.send(data);         // automatic -> application/json
-	});
-});
-
-//socket stuff
-io.on('connection', function(socket){
-    console.log('a user connected to socketIO');
-    //send the device Id to web client
-    io.emit('data_wclient_init', devId);
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-    socket.on('shutdown', function() {
-            child = exec("shutdown now");
-    });
-    //handle data recieved from other LoRa devices
-    socket.on('data_sx127x', function(data) {
-       
-        console.log('recieved data_sx127x:',data);
-        /* 
-        console.log('deviceID: ',data[0]);
-        console.log('lat :', hexToInt(bytesToHex(data.slice(1,5))) / 10000000)
-        console.log('lon :', hexToInt(bytesToHex(data.slice(5,9))) / 10000000) */
-        console.log('data_sx127x.length', data.length)
-        recievedPayload = decodePayload(data);
-    
-        localTable = updateLocalTable(recievedTable,localTable);
-        
-        //data for webclient
-        var data = {
-            'devId' : data[0],
-            'lat': (hexToInt(bytesToHex(data.slice(1,5))) / 10000000),
-            'lon': (hexToInt(bytesToHex(data.slice(5,9))) / 10000000),
-            'time': new Date()
-        };
-        io.emit('data_web', data);
-    });
-});
-
-http.listen(port);
-
-console.log('server started on port %s', port);
-
-
-
-//SQL
-var sqlite3 = require('sqlite3');
-TransactionDatabase = require("sqlite3-transactions").TransactionDatabase;
-// Setup database connection for logging
-var db = new TransactionDatabase(
-    new sqlite3.Database("/projects/mercury/mercury_maps/mercery.db", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE)
-);
-
-// Start socket server python scripts
-// executes `pkill` - make sure socketserver isnt already running
-child = exec("pkill -f sendRecieveData.py");
-
-var PythonShell = require('python-shell');
-var pyshell = new PythonShell('./pySX127x/sendRecieveData.py',{args: ['freq --433']},function (err, results) {
-  if (err) throw err;
-  // results is an array consisting of messages collected during execution
-  console.log('results: %j', results);
-});
-pyshell.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement)
-  console.log(message);
-});
-// end the input stream and allow the process to exit
-pyshell.end(function (err) {
-  if (err) throw err;
-  console.log('finished');
-});
-//start GPSD
-// executes `pkill` - make sure socketserver isnt already running
-child = exec("pkill -f gpsd");
-
-var gpsd = require('node-gpsd');
-var gpsd = require('./lib/gpsd.js');
-var previousGpsdTime = new Date(); //keep track of the last time we got a GPSD cvalue
-var GpsdInterval = 5;//how often (seconds) to use GPSD data
-var daemon = new gpsd.Daemon({
-    program: 'gpsd',
-    device: '/dev/ttyAMA0',
-    port: 2947,
-    pid: '/tmp/gpsd.pid',
-    logger: {
-        info: function() {},
-        warn: console.warn,
-        error: console.error
-    }
-});
-daemon.start(function() {
-    console.log('Started GPSD daemon');
-	var listener = new gpsd.Listener();
-    listener.on('TPV', function (tpv) {
-		//only collect data every GpsdInterval
-        //console.log(tpv);
-        //tpv.mode should be 3 since 3 means lot/lat
-		if(((new Date().getTime() - previousGpsdTime.getTime()) / 1000) > GpsdInterval && tpv.time && tpv.lat && tpv.lon && tpv.alt && tpv.speed){
-            previousGpsdTime = new Date();
-			var statement = db.prepare("INSERT INTO GpsLog VALUES (?, ?, ?, ?, ?)");
-			// Insert values into prepared statement
-			var lat = tpv.lat;
-			var lon = tpv.lon;
-            var time = Date.parse(tpv.time);
-            //console.log('parsedTpvDatetime', time)
-			statement.run(time, lat, lon, tpv.alt, tpv.speed );
-			// Execute the statement
-			statement.finalize();
-			//send info to Python socket / to broadcast
-            //init payload to send
-    /*         var payload = new Array()
-            //Change Lat / Lon to a non decimal number
-            lat = Math.round(tpv.lat * 10000000)
-            lon = Math.round(tpv.lon * 10000000)
-            //convert decimal to hex string
-            latHexStr = decimalToHexString(lat)
-            lonHexStr = decimalToHexString(lon)
-            
-            //convert hex string to byte array
-            //Convert signed hex lon to int / lon value
-            latByte =  hexToBytes(latHexStr)
-            lonByte =  hexToBytes(lonHexStr)
-
-            //put together payload
-            payload[0] = devId //add device id to the first bye
-            //fill out longitude in payload
-            for(var i = 0; i < latByte.length; i++){
-               payload[i + 1]  = latByte[i]
-            }
-            //fill out latitude in payload
-            for(var i = 0; i < lonByte.length; i++){
-               payload[i + 1 + latByte.length]  = lonByte[i]
-            } */
-            
-            //add new GPS units to local table
-            DataFromGps(devId, lat, lon, time);
-            //convert localtable to payload
-            //console.log("localtable:", localTable)
-            payload = localTableToPaload(localTable);
-            //Send payload
-            console.log('sendpayload.length',payload.length);
-            console.log('sending data_sx127x:',payload);
-            
-			io.emit('data_sx127x', payload);
-            
-            
-            //data for webclient
-            var data = {
-                'devId': devId,
-                'lat':tpv.lat,
-                'lon':tpv.lon,
-                'time':new Date()
-            };
-            io.emit('data_web', data);
-		}
-    });
-    listener.connect(function() {
-        listener.watch();
-    });
-});
-
-
 
 
 
